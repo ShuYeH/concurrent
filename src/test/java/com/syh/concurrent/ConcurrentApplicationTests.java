dae6e2b12fe936196ff4a280cb7b9bb03cdb6d3b
ShuYeH <729088164@qq.com>
2020-03-25 11:15:58 +0800

    'init'

---
+++ src/test/java/com/syh/concurrent/ConcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syh.concurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
